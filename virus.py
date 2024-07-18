9fb95f507db3878966d13ae23b80baabb37773a8
jacode21cf <jacode241@gmail.com>
2024-07-18 09:01:24 +0200

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,13 @@
+from os import system
+import os
+
+code1 = input("introduce el codigo1:")
+code2 = input("introduce el codigo2:")
+code3 = input("introduce el codigo3:")
+code4 = input("introduce el codigo4:")
+
+codes = code1,code2,code3,code4
+
+
+def exect():
+    
